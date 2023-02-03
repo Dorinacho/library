3ac26aaffef1eb1e3a0117ff031f6229b84773c2
DoSuciu <Dorin.Suciu@endava.com>
2023-02-03 17:05:11 +0200

    Created repositories

---
+++ backend/src/main/java/com/library/repository/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.library.repository;
+
+import com.library.model.Loan;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends CrudRepository<Loan, Long> {
+}
---
+++ backend/src/main/java/com/library/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.library.repository;
+
+import com.library.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
